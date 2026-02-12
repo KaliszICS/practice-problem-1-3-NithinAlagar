824b66c185d37bb64285c453f56cb95a50eb5740
NithinAlagar <343954178@tdsb.ca>
2026-02-12 16:27:49 +0000

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,9 @@
+/**
+* File Name: Lesson 1.3 - Strings
+* Author: Nithin Alagar
+* Date Created: Feb 8, 2023
+* Date Last Modified: Feb 12, 2026
+*/
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +15,32 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+		System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+		System.out.println("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
+
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		String cow = "          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''";
+		System.out.println(cow);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		String word1 = "Fortnite ";
+		String word2 = "is awful\n";
+		System.out.print(word1);
+		System.out.print(word2);
+
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		String word3 = "Don't";
+		String word4 = " @ me";
+		System.out.println(word3 + word4);
 	}
 
 
